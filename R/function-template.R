53a6983a018c387d9007f8c82f901674aa6a4cdb
joethorley <joe@poissonconsulting.ca>
2019-07-31 12:51:34 -0700

    chk as dependency

--- R/function-template.R
+++ R/function-template.R
@@ -3,13 +3,16 @@
 #' A function template.
 #' 
 #' @param x A flag specifying whether to return TRUE or FALSE.
+#' @param chk A flag specifying whether to check inputs.
 #' @return A flag.
 #' @export
 #'
 #' @examples
 #' function_template()
 #' function_template(FALSE)
-function_template <- function(x = TRUE) {
-  check_flag(x)
+function_template <- function(x = TRUE, chk = TRUE) {
+  if(isTRUE(chk)) {
+    chk_flag(x)
+  }
   x
 }
