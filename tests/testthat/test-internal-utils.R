bbe7925f93dbd651e442a15ddd66aae7dd4f5745
joethorley <joe@poissonconsulting.ca>
2019-08-02 13:24:11 -0700

    test string extract

--- tests/testthat/test-internal-utils.R
+++ tests/testthat/test-internal-utils.R
@@ -34,3 +34,18 @@ test_that("remove_nulls",{
   expect_identical(remove_nulls(list(NULL)), list())
   expect_identical(remove_nulls(list(x = 1, y = NULL)), list(x = 1))
 })
+
+test_that("str_extract",{
+  expect_error(str_extract(character(0), character(0)), 
+               "^invalid 'pattern' argument$")
+  
+  expect_identical(str_extract(character(0), ".*"), character(0))
+  expect_identical(str_extract(NA_character_, ".*"), character(0))
+  expect_identical(str_extract("a", ".*"), "a")
+  expect_identical(str_extract(c("a", NA), ".*"), "a")
+  expect_identical(str_extract(c("a", "b"), ".*"), c("a", "b"))
+  expect_identical(str_extract(c("aa a", "bbb"), "\\w"), c("a", "b"))
+  expect_identical(str_extract(c("aa a", "bbb"), "\\w\\w"), c("aa", "bb"))
+  expect_identical(str_extract(c("aa a", "bbb"), "\\w+"), c("aa", "bbb"))
+  expect_identical(str_extract(c("aa a", "bbb"), "\\s\\w+"), " a")
+})
